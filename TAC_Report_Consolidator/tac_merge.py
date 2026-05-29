7a4db4a50d12b3bf6fb621e75b6f4c9028e96671
ahmedf <farhanahmedch@gmail.com>
2026-05-29 16:12:28 -0700

    Read from file, write in proper way in merged file.

--- TAC_Report_Consolidator/tac_merge.py
+++ TAC_Report_Consolidator/tac_merge.py
@@ -26,23 +26,35 @@ def command_name_from_filename(filename):
         filename = filename.split("_", 1)[1] # splitting the initial number part from filename.
     return filename
 
-# --------------------------------------------------
+input_file = "tac_report.tar.gz"
+output_file = Path(f"TAC_Report_Merged_{timestamp()}.txt")
 
-archive = "tac_report.tar.gz"
+with tarfile.open(input_file, "r:*") as tar:
+    with open(output_file, "w", encoding="utf-8") as out:
+        for member in tar.getmembers():
+            filename = PurePosixPath(member.name).name
 
-with tarfile.open(archive, "r:*") as tar:
-    for member in tar.getmembers():
-        filename = PurePosixPath(member.name).name
+            # If file name is NOT ending with .log then skip it.
+            if not filename.endswith(".log"):
+                continue
 
-        if filename.endswith(".log"):
+            # Stripping the extension and number prefix.
+            command_name = command_name_from_filename(filename)
+
+            # Read content of the TAR members i.e. log files.
             file_obj = tar.extractfile(member)
+            # Skip if file cannot be opened or its a folder
+            if file_obj is None:
+                continue
+            # read the file, decode it to text and strip extra blank lines/spaces at the end.
+            content = file_obj.read().decode("utf-8", errors="replace").rstrip()
 
-            if file_obj:
-                data = file_obj.read()
-                text = data.decode("utf-8", errors="replace")
-                print (text)
-                break
+            # Write to the output file.
+            out.write("### " + command_name + "\n\n")
+            out.write(content)
+            out.write("\n\n\n")
 
+print(f"Created: {output_file}")
 
 
 # -----
