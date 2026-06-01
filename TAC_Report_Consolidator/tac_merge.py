0a840eddbfde3f479584e85258599b41b44b8138
ahmedf <farhanahmedch@gmail.com>
2026-06-01 10:02:09 -0700

    argparse

--- TAC_Report_Consolidator/tac_merge.py
+++ TAC_Report_Consolidator/tac_merge.py
@@ -6,6 +6,7 @@ Author: Farhan Ahmed - www.farhan.ch
 """
 
 import tarfile
+import argparse
 from datetime import datetime
 from pathlib import Path, PurePosixPath
 
@@ -21,13 +22,41 @@ def timestamp():
 
 def command_name_from_filename(filename):
     if filename.endswith(".log"):
-        filename = filename[:-4]  # GUI TAC report files are always ending on .log, hence -4 will work.
+        # GUI TAC report files are always ending on .log, hence -4 will work below.
+        filename = filename[:-4]
     if "_" in filename:
-        filename = filename.split("_", 1)[1] # splitting the initial number part from filename.
+        # splitting the initial number part from filename.
+        filename = filename.split("_", 1)[1]
     return filename
 
-input_file = "tac_report.tar.gz"
-output_file = Path(f"TAC_Report_Merged_{timestamp()}.txt")
+# Adding argparse to select the tar archive file
+parser = argparse.ArgumentParser(
+    description="TACMerge - merge FMG/FAZ GUI TAC Report"
+)
+# select an input file
+parser.add_argument(
+    "input_file",
+    help="Path to TAC archive file"
+)
+# Output directory is optional, if not entered default is current dir.
+parser.add_argument(
+    "-o",
+    "--output_file",
+    default=".",
+    help="Output folder. Default is current directory."
+)
+
+args = parser.parse_args()
+
+input_file = args.input_file
+# converting to Path.
+output_dir = Path(args.output_file)
+# If output_dir does not exist then create it.
+# parents=True  => if parent folders (nested) do not exist then create them too.
+# exist_ok=True => if folder already exist, do not crash.
+output_dir.mkdir(parents=True, exist_ok=True)
+# create output_file in the output_dir.
+output_file = output_dir / f"TAC_Report_Merged_{timestamp()}.txt"
 
 with tarfile.open(input_file, "r:*") as tar:
     with open(output_file, "w", encoding="utf-8") as out:
@@ -43,7 +72,7 @@ with tarfile.open(input_file, "r:*") as tar:
 
             # Read content of the TAR members i.e. log files.
             file_obj = tar.extractfile(member)
-            # Skip if file cannot be opened or its a folder
+            # Skip if file cannot be opened or it's a folder
             if file_obj is None:
                 continue
             # read the file, decode it to text and strip extra blank lines/spaces at the end.
@@ -55,9 +84,3 @@ with tarfile.open(input_file, "r:*") as tar:
             out.write("\n\n\n")
 
 print(f"Created: {output_file}")
-
-
-# -----
-# MAIN
-# ------
-print("\n### " + (filename))
